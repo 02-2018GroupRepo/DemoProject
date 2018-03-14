0fc647d969d896afdf7ff30cae6cf33f83d6367b
Robert Kee <roberthayeskee@gmail.com>
2018-03-14 11:20:38 -0400

    Update Machine.java

--- src/com/demoproject/Machine.java
+++ src/com/demoproject/Machine.java
@@ -1,4 +1,15 @@
 package com.demoproject;
 
 public class Machine {
+  
+  private double totalMoney;
+  
+  public Machine() {
+  totalMoney = 35d;
+  }
+  
+  public double getMachineMoney() {
+    return totalMoney;
+  }
+  
 }
