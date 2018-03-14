b18cf66cdd12180e8c0f322f13ae52915c854cea
Robert Kee <roberthayeskee@gmail.com>
2018-03-14 11:24:23 -0400

    Update Main.java

--- src/com/demoproject/Main.java
+++ src/com/demoproject/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+	    Machine demoMachine = new Machine();
+	    System.out.println("Machine has $" + machine.getMachineMoney()); 
     }
 }
